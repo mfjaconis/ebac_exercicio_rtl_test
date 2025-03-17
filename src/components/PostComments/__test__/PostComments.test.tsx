2d524129a789cc731327713664036c1cd0993a0d
mfjaconis <matheusjaconis@gmail.com>
2025-03-17 15:55:09 -0300

    testes

--- src/components/PostComments/__test__/PostComments.test.tsx
+++ src/components/PostComments/__test__/PostComments.test.tsx
@@ -1,9 +1,24 @@
-import { render, screen } from "@testing-library/react";
-import PostComment from "..";
+import { fireEvent, render, screen } from "@testing-library/react";
+import Post from "..";
 
-describe("Teste para o componente PostComment", () => {
-	test("Deve renderizar o componente corretamente", () => {
-		render(<PostComment />);
-		expect(screen.getByText("Comentar")).toBeInTheDocument();
+describe("Tests for PostComments component", () => {
+	test("Must add two comments", () => {
+		render(<Post />);
+		const input = screen.getByTestId("textarea-input");
+		const button = screen.getByTestId("btn-submit");
+		fireEvent.change(input, {
+			target: {
+				value: "Muito bom!",
+			},
+		});
+		fireEvent.click(button);
+		fireEvent.change(input, {
+			target: {
+				value: "Foi incrível!",
+			},
+		});
+		fireEvent.click(button);
+		expect(screen.getByText("Muito bom!")).toBeInTheDocument();
+		expect(screen.getByText("Foi incrível!")).toBeInTheDocument();
 	});
 });
